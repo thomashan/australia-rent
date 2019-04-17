4f24b3a7e96caab08c4402ee5d8df0c8de992139
Thomas Han <thomas.han@live.com>
2019-04-18 00:58:57 +1000

    added google geo coder

---
+++ src/main/groovy/thomashan/github/io/australia/rent/geocode/google/GoogleGeocoder.groovy
@@ -0,0 +1,32 @@
+package thomashan.github.io.australia.rent.geocode.google
+
+
+import com.google.maps.GeoApiContext
+import com.google.maps.GeocodingApi
+import com.google.maps.model.GeocodingResult
+import thomashan.github.io.australia.rent.LatLongCoordinates
+import thomashan.github.io.australia.rent.RentDetails
+import thomashan.github.io.australia.rent.geocode.Geocoder
+
+class GoogleGeocoder implements Geocoder {
+    private static final String GOOGLE_API_KEY = System.getenv("GOOGLE_API_KEY")
+    GeoApiContext geoApiContext = new GeoApiContext.Builder()
+            .apiKey(GOOGLE_API_KEY)
+            .build()
+
+
+    @Override
+    List<RentDetails> geocode(List<RentDetails> rentDetails) {
+        return rentDetails.collect {
+            if (!it.coordinates.empty) {
+                return it
+            } else {
+                GeocodingResult[] geocodingResults = GeocodingApi.geocode(geoApiContext, "${it.address}, ${it.suburb}, ${it.state} ${it.postcode}").await()
+                GeocodingResult geocodingResult = geocodingResults[0]
+                LatLongCoordinates latLongCoordinates = new LatLongCoordinates(geocodingResult.geometry.location.lat, geocodingResult.geometry.location.lng)
+
+                return new RentDetails(it.price, it.address, it.suburb, it.state, it.postcode, Optional.of(latLongCoordinates))
+            }
+        }
+    }
+}
---
+++ src/test/groovy/thomashan/github/io/australia/rent/geocode/google/GoogleGeocoderSpec.groovy
@@ -0,0 +1,37 @@
+package thomashan.github.io.australia.rent.geocode.google
+
+import spock.lang.Specification
+import thomashan.github.io.australia.rent.LatLongCoordinates
+import thomashan.github.io.australia.rent.RentDetails
+
+class GoogleGeocoderSpec extends Specification {
+    private GoogleGeocoder geocoder = new GoogleGeocoder()
+
+    def "should populate latitude and longitude if initial latitude longitude is empty"() {
+        given:
+        RentDetails rentDetails1 = new RentDetails(Optional.of(1), "1 Collins St", "Melbourne", "VIC", "3000", Optional.empty())
+        List<RentDetails> initialRentDetails = [rentDetails1]
+
+        when:
+        List<RentDetails> resultantRentDetails = geocoder.geocode(initialRentDetails)
+        RentDetails resultantRentDetails1 = resultantRentDetails[0]
+
+        then:
+        resultantRentDetails1.coordinates.get().latitude == -37.8138267
+        resultantRentDetails1.coordinates.get().longitude == 144.973728
+    }
+
+    def "should not hit google api if latitude and longitude exists"() {
+        given:
+        RentDetails rentDetails1 = new RentDetails(Optional.of(1), "1 Collins St", "Melbourne", "VIC", "3000", Optional.of(new LatLongCoordinates(-37.8138267, 144.973728)))
+        List<RentDetails> initialRentDetails = [rentDetails1]
+
+        when:
+        List<RentDetails> resultantRentDetails = geocoder.geocode(initialRentDetails)
+        RentDetails resultantRentDetails1 = resultantRentDetails[0]
+
+        then:
+        resultantRentDetails1.coordinates.get().latitude == -37.8138267
+        resultantRentDetails1.coordinates.get().longitude == 144.973728
+    }
+}

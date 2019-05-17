f01466f5a5f0642083dc97e22b82af11d0099c2e
Thomas Han <thomas.han@live.com>
2019-05-18 01:26:41 +1000

    run chrome in headless mode

--- src/main/resources/GebConfig.groovy
+++ src/main/resources/GebConfig.groovy
@@ -1,7 +1,15 @@
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
-import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver
+//import org.openqa.selenium.firefox.FirefoxDriver
 
 //driver = { new FirefoxDriver() }
-driver = { new HtmlUnitDriver() }
+//driver = { new HtmlUnitDriver() }
+driver = {
+    ChromeOptions chromeOptions = new ChromeOptions()
+    chromeOptions.addArguments("headless")
 
-reportOnTestFailureOnly=true
+    return new ChromeDriver(chromeOptions)
+}
+
+reportOnTestFailureOnly = true

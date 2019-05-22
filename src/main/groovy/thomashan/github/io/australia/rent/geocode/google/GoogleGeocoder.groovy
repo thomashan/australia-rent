1b276add1f54c83dffcd05ece95a1a2b8807d7cd
Thomas Han <thomas.han@live.com>
2019-05-23 02:04:08 +1000

    drop the unit number of the initial geocoding is a failure

--- src/main/groovy/thomashan/github/io/australia/rent/geocode/google/GoogleGeocoder.groovy
+++ src/main/groovy/thomashan/github/io/australia/rent/geocode/google/GoogleGeocoder.groovy
@@ -8,9 +8,13 @@ import thomashan.github.io.australia.rent.LatLongCoordinates
 import thomashan.github.io.australia.rent.RentDetails
 import thomashan.github.io.australia.rent.geocode.Geocoder
 
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 class GoogleGeocoder implements Geocoder {
     private static final String GOOGLE_API_KEY = System.getenv("GOOGLE_API_KEY")
     private GeoApiContext geoApiContext
+    private Pattern dropUnitPattern = ~/\d*\/(\d.*)/
 
     GoogleGeocoder() {
         if (!GOOGLE_API_KEY) {
@@ -31,11 +35,39 @@ class GoogleGeocoder implements Geocoder {
                 String fullAddress = "${it.address}, ${it.suburb}, ${it.state} ${it.postcode}"
                 println("Geocoding: ${fullAddress}")
                 GeocodingResult[] geocodingResults = GeocodingApi.geocode(geoApiContext, fullAddress).await()
-                GeocodingResult geocodingResult = geocodingResults[0]
-                LatLongCoordinates latLongCoordinates = new LatLongCoordinates(geocodingResult.geometry.location.lat, geocodingResult.geometry.location.lng)
+                if (geocodingResults.size() > 0) {
+                    GeocodingResult geocodingResult = geocodingResults[0]
+                    LatLongCoordinates latLongCoordinates = new LatLongCoordinates(geocodingResult.geometry.location.lat, geocodingResult.geometry.location.lng)
 
-                return new RentDetails(it.price, it.address, it.suburb, it.state, it.postcode, it.bedrooms, it.bathrooms, it.parking, Optional.of(latLongCoordinates))
+                    return new RentDetails(it.price, it.address, it.suburb, it.state, it.postcode, it.bedrooms, it.bathrooms, it.parking, Optional.of(latLongCoordinates))
+                } else {
+                    String fullAddress1 = "${dropUnit(it.address)}, ${it.suburb}, ${it.state} ${it.postcode}"
+                    println("Geocoding: ${fullAddress1}")
+
+                    GeocodingResult[] geocodingResults1 = GeocodingApi.geocode(geoApiContext, fullAddress1).await()
+
+                    if (geocodingResults1.size() > 0) {
+                        GeocodingResult geocodingResult1 = geocodingResults1[0]
+                        LatLongCoordinates latLongCoordinates = new LatLongCoordinates(geocodingResult1.geometry.location.lat, geocodingResult1.geometry.location.lng)
+
+                        return new RentDetails(it.price, it.address, it.suburb, it.state, it.postcode, it.bedrooms, it.bathrooms, it.parking, Optional.of(latLongCoordinates))
+                    }
+
+                    return it
+                }
             }
         }
+    }
+
+    private String dropUnit(String address) {
+        Matcher matcher = dropUnitPattern.matcher(address)
+
+        if (matcher.size() > 0) {
+            int size = matcher[0].size()
+
+            return matcher[0][size - 1]
+        }
+
+        return address
     }
 }
--- src/test/groovy/thomashan/github/io/australia/rent/geocode/google/GoogleGeocoderSpec.groovy
+++ src/test/groovy/thomashan/github/io/australia/rent/geocode/google/GoogleGeocoderSpec.groovy
@@ -7,6 +7,20 @@ import thomashan.github.io.australia.rent.RentDetails
 class GoogleGeocoderSpec extends Specification {
     private GoogleGeocoder geocoder = new GoogleGeocoder()
 
+    def "should drop the unit if the initial geocoding was not successful"() {
+        given:
+        RentDetails rentDetails1 = new RentDetails(Optional.of(1), "2/606 Waverley Rd", "GLEN WAVERLEY", "VIC", "3150", 0, 0, 0, Optional.empty())
+        List<RentDetails> initialRentDetails = [rentDetails1]
+
+        when:
+        List<RentDetails> resultantRentDetails = geocoder.geocode(initialRentDetails)
+        RentDetails resultantRentDetails1 = resultantRentDetails[0]
+
+        then:
+        resultantRentDetails1.coordinates.get() == -37.887699
+        resultantRentDetails1.coordinates.get().longitude == 145.152062
+    }
+
     def "should populate latitude and longitude if initial latitude longitude is empty"() {
         given:
         RentDetails rentDetails1 = new RentDetails(Optional.of(1), "1 Collins St", "Melbourne", "VIC", "3000", 0, 0, 0, Optional.empty())

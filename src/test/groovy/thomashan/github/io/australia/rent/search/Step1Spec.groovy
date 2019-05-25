77e19877cfd029eb632e6afd940cb25d0ec5b84e
Thomas Han <thomas.han@live.com>
2019-05-26 02:27:27 +1000

    enable step 1 spec until we have a main runner app

--- src/test/groovy/thomashan/github/io/australia/rent/search/Step1Spec.groovy
+++ src/test/groovy/thomashan/github/io/australia/rent/search/Step1Spec.groovy
@@ -1,10 +1,8 @@
 package thomashan.github.io.australia.rent.search
 
 import geb.spock.GebReportingSpec
-import spock.lang.Ignore
 
 class Step1Spec extends GebReportingSpec {
-    @Ignore
     def "search test"() {
         when:
         Step1 step1 = new Step1()

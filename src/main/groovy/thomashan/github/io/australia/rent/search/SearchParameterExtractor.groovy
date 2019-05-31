65e52316e8759ed969d40dc8e030a6cd811e6ac5
Thomas Han <thomas.han@live.com>
2019-06-01 01:43:51 +1000

    added search parameter extractor

---
+++ src/main/groovy/thomashan/github/io/australia/rent/search/SearchParameterExtractor.groovy
@@ -0,0 +1,43 @@
+package thomashan.github.io.australia.rent.search
+
+final class SearchParameterExtractor {
+    private static final emptyString = ""
+
+    private SearchParameterExtractor() {
+        // prevent instantiation
+    }
+
+    static String suburbs(SearchQuery searchQuery) {
+        return searchQuery.suburbs.empty || searchQuery.suburbs.get().empty ? "melbourne-region-vic" : searchQuery.suburbs.get().join(",")
+    }
+
+    static String bedrooms(SearchQuery searchQuery) {
+        switch (searchQuery) {
+            case { it.minBedroom.empty && it.maxBedroom.empty }:
+                return emptyString
+            case { !it.minBedroom.empty && !it.maxBedroom.empty }:
+                return "${searchQuery.minBedroom.get()}-${searchQuery.maxBedroom.get()}"
+            case { !it.minBedroom.empty && it.maxBedroom.empty }:
+                return "${searchQuery.minBedroom.get()}-any"
+            case { it.minBedroom.empty && !it.maxBedroom.empty }:
+                return "0-${searchQuery.maxBedroom.get()}"
+            default:
+                throw new RuntimeException("shouldn't get here")
+        }
+    }
+
+    static String prices(SearchQuery searchQuery) {
+        switch (searchQuery) {
+            case { it.minPrice.empty && it.maxPrice.empty }:
+                return emptyString
+            case { !it.minPrice.empty && !it.maxPrice.empty }:
+                return "${searchQuery.minPrice.get()}-${searchQuery.maxPrice.get()}"
+            case { !it.minPrice.empty && it.maxPrice.empty }:
+                return "${searchQuery.minPrice.get()}-any"
+            case { it.minPrice.empty && !it.maxPrice.empty }:
+                return "0-${searchQuery.maxPrice.get()}"
+            default:
+                throw new RuntimeException("shouldn't get here")
+        }
+    }
+}

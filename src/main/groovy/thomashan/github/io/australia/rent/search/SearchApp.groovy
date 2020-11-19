6899331b197994bbaa7d36022009eac862943f6e
Thomas Han <thomas.han@live.com>
2020-11-19 23:18:15 +1100

    search for exactly 3 bedroom as 3+ was getting to many details to fit into the 50 page restriction

--- src/main/groovy/thomashan/github/io/australia/rent/search/SearchApp.groovy
+++ src/main/groovy/thomashan/github/io/australia/rent/search/SearchApp.groovy
@@ -5,7 +5,7 @@ import static java.util.Optional.of
 
 class SearchApp {
     static void main(String[] args) {
-        new Step1(new SearchQuery(e(), of(550), of(650), of(3), e())).search()
+        new Step1(new SearchQuery(e(), of(550), of(650), of(3), of(3))).search()
         new Step1(new SearchQuery(e(), of(700), of(750), of(3), e())).search()
     }
 }

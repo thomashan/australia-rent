a541748aab66d62c3469a425c660b7b3c5b90231
Thomas Han <thomashan@users.noreply.github.com>
2020-11-25 23:38:04 +1100

    Feature/issue 24 fix timeout issue (#26)
    * issue-24: more finely grained search

--- src/main/groovy/thomashan/github/io/australia/rent/search/SearchApp.groovy
+++ src/main/groovy/thomashan/github/io/australia/rent/search/SearchApp.groovy
@@ -5,7 +5,12 @@ import static java.util.Optional.of
 
 class SearchApp {
     static void main(String[] args) {
-        new Step1(new SearchQuery(e(), of(550), of(650), of(3), of(3))).search()
+        new Step1(new SearchQuery(e(), of(400), of(450), of(3), of(3))).search()
+        new Step1(new SearchQuery(e(), of(450), of(500), of(3), of(3))).search()
+        new Step1(new SearchQuery(e(), of(500), of(550), of(3), of(3))).search()
+        new Step1(new SearchQuery(e(), of(550), of(600), of(3), of(3))).search()
+        new Step1(new SearchQuery(e(), of(600), of(650), of(3), of(3))).search()
+        new Step1(new SearchQuery(e(), of(650), of(700), of(3), of(3))).search()
         new Step1(new SearchQuery(e(), of(700), of(750), of(3), e())).search()
     }
 }
